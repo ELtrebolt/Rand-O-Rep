52e8c3b8ee18b114207cb0ffd7799c7e2cd8ef14
David <davidvd@uci.edu>
2022-12-06 21:06:14 -0800

    RANDOM REST UPDATES WOO GO TEAM

--- src/app/random-rest/random-rest.component.ts
+++ src/app/random-rest/random-rest.component.ts
@@ -1,26 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-random-rest',
   templateUrl: './random-rest.component.html',
   styleUrls: ['./random-rest.component.css']
 })
-export class RandomRestComponent {
+export class RandomRestComponent implements OnInit, OnChanges{
   periods:String[] = ["1 minute", "2 minutes", "3 minutes"];
   @Input() gesture:String = "None";
   chosen_period:String = "";
 
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.gesture) {
+      const currValue = changes.gesture.currentValue;
+      if(currValue == "Hand Pointing")
+      {
+        this.chosen_period = this.periods[Math.floor(Math.random()*this.periods.length)];
+      }
+      else if(currValue == "None")
+      {
+        this.chosen_period = "Point Hand to Get a Random Rest Period!"
+      }
+    }
+  }
+
   ngOnInit(): void {
-    if(this.gesture == "Hand Pinching")
-    {
-      this.chosen_period = this.periods[Math.floor(Math.random()*this.periods.length)];
-    }
-    else if(this.gesture == "None")
-    {
-      this.chosen_period = "Point Hand to Get a Random Rest Period!"
-    }
+    
   }
 
   update(): void {
